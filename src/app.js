1e0438c7d6ab1f7adf19f980829db7e007ff0a5b
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2025-01-08 22:18:52 +0600

    cors access to all

--- src/app.js
+++ src/app.js
@@ -12,11 +12,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: [
-            "https://artistweb-tau.vercel.app",
-            "http://localhost:3000",
-            "http://localhost:3001",
-        ],
+        origin: "*",
         credentials: true,
     })
 );
